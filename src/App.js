2c027ebc0618259eed18764cfab2b70a58f838ca
Tony Lauf <LAU5619@Tonys-MacBook-Pro.local>
2018-01-28 15:41:23 -0600

    Complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
 import axios from 'axios'
+import './ui-toolkit/css/nm-cx/main.css'
 
 class App extends Component {
 
@@ -28,7 +29,7 @@ class App extends Component {
   onMovieIn({target}) {
     console.log(target)
     if (target.value.length === 0) {
-      this.setState({ movieVal: target.value, movieErr: true, movieMsg: 'Please enter a movie to search on.', movieSuccess: false, disableSbmBtn: true})
+      this.setState({ movieVal: target.value, movieErr: true, movieMsg: 'Please enter a movie to search on.', movieSuccess: false, movieRspMsg: '', disableSbmBtn: true})
     } else {
       this.setState({ movieVal: target.value, movieErr: false, movieMsg: '', movieSuccess: false, disableSbmBtn: false})
     }
@@ -45,7 +46,7 @@ class App extends Component {
     promise.then(response => {
       console.log(response)
       if (response.data.hasOwnProperty('Error')){
-        let movieRspErr = "Request failed with status: " + response.data.Response + " with message: " + response.data.Error
+        let movieRspErr = "Request failed with status: " + response.data.Response + " and message: " + response.data.Error
         this.setState({ movieSuccess: false, movieRspMsg: movieRspErr })
       } else {
         this.setState({ movieSuccess: true, movieRspMsg: '', movieYear: response.data.Year, movieDir: response.data.Director, moviePlot: response.data.Plot })
@@ -53,7 +54,7 @@ class App extends Component {
     });
     promise.catch(error => {
       console.log(error)
-      let movieRspErr = "Request failed with status: " + error.response.status + " with message: " + error.response.data.message
+      let movieRspErr = "Request failed with status: " + error.response.status + " and message: " + error.response.data.message
       this.setState({ movieSuccess: false, movieRspMsg: movieRspErr })
     });
   }
@@ -67,40 +68,48 @@ class App extends Component {
         <div class="card">
           <form onSubmit={this.onSbmClick}>
             <div class="row">
-              <div class="small-4 columns md-text-field with-floating-label">
-                <input type="text" id="movie_in" value={this.state.movieVal} onChange={this.onMovieIn} />
-                <label for="movie_in"></label>
-                <span class="error">{this.state.movieMsg}</span>
+              <div class="small-12 columns">
+                <div class="small-4 columns md-text-field with-floating-label icon-left">
+                  <input type="search" id="movie_in" placeholder='Enter the  movie title to search for.' value={this.state.movieVal} onChange={this.onMovieIn} />
+                  <label for="movie_in"></label>
+                  <span class="error">{this.state.movieMsg}</span>
+                  <span class="icon icon-sysicon-search"></span>
+                </div>
+                <div class="small-1 columns padding-small">
+                  <button class="button btn-cta" disabled={this.state.disableSbmBtn} onChange={this.onSbmClick}>Search</button>
+                </div>
+                <div class="small-8 columns" ></div>
               </div>
             </div>
-            <div class="row padding-small">
-              <button class="button btn-cta" disabled={this.state.disableSbmBtn} onChange={this.onSbmClick}>Search</button>
-            </div>
+          </form>  
+        </div>          
             {this.state.movieSuccess &&
-            <div class="row">
-              <div class="row">
-                <span class="label small">Year: </span>
-                <span id="movieYear" class=".small-1" >{this.state.movieYear}</span>
+              <div class="card">
+                <div class="row">
+                  <div class="row">
+                    <div id="movieYear" class="large-2 columns"><span class="label small">Year: </span>{this.state.movieYear}</div>
+                    <div class="large-10 columns" ></div>
+                  </div>
+                  <div class="row">
+                    <div id="movieDir" class="large-6 columns" ><span class="label small">Directors: </span>{this.state.movieDir}</div>
+                    <div class="large-6 columns" ></div>
+                  </div>
+                  <div class="row">
+                    <div id="moviePlot" class="large-12 columns" ><span class="label small">Plot: </span>{this.state.moviePlot}</div>
+                  </div>
+                </div>
               </div>
-              <div class="row">
-                <span class="label small">Director: </span>
-                <span id="movieDir" class=".small-1" >{this.state.movieDir}</span>
-              </div>
-              <div class="row">
-                <span class="label small">Plot: </span>
-                <span id="moviePlot" class=".small-1" >{this.state.moviePlot}</span>
-              </div>
-            </div>
             }
             {!this.state.movieSuccess &&
+            <div class="card">
               <div class="row">
-                <span class="label small"></span>
-              <span id="movieRspErr" class=".small-1" >{this.state.movieRspMsg}</span>
+                <div class="row">
+                  <div id="movieRspMsg" class="large-12 columns" >{this.state.movieRspMsg}</div>
+                </div>
               </div>
+            </div>
             }
-          </form>
         </div>
-      </div>
     );
   }
 }
